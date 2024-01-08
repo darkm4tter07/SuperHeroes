f5f1c96806f8967c620a9afda36b1a113e5bd811
darkmatter <vikasanand.darkmatter@gmail.com>
2024-01-08 19:26:26 +0530

    Search Button changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import '../css/Navbar.css';
 
-function Navbar({onSearch}) {
-    const [searchText, setSearchText] = useState("");
+function Navbar({ onSearch }) {
+  const [searchText, setSearchText] = useState("");
 
-    const handleSearch = () => {
-        onSearch(searchText);
-        setSearchText("");
+  const handleSearch = () => {
+    onSearch(searchText);
+    setSearchText("");
+  };
+
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
     }
+  };
 
   return (
     <div className='header'>
@@ -15,8 +25,17 @@ function Navbar({onSearch}) {
         <h1 className='header__name'>SuperHeroes</h1>
       </div>
       <div className="search__character">
-        <input className='search__bar' type='search' placeholder='Character Name' value={searchText} onSearch={(event)=>{setSearchText(event.target.value)}} />
-        <button className='search__button' onClick={handleSearch}>Search</button>
+        <input
+          className='search__bar'
+          type='search'
+          placeholder='Character Name'
+          value={searchText}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+        />
+        <button className='search__button' onClick={handleSearch}>
+          Search
+        </button>
       </div>
     </div>
   );
